a694164c35b71838ea18df04e9f55ed1eb6a44fd
StaragileDevops <116961807+StaragileDevops@users.noreply.github.com>
2023-03-08 16:37:15 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,58 @@
+terraform {
+  required_providers {
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+      version = "2.11.0"
+    }
+  }
+}
+
+provider "kubernetes" {
+  config_path    = "~/.kube/config"
+  config_context = "minikube"
+}
+resource "kubernetes_namespace" "medicure" {
+  metadata {
+    name = "minikube-medicure"
+  }
+}
+
+resource "kubernetes_deployment" "medicure" {
+  metadata {
+    name = "medicure-example"
+    labels = {
+      test = "medicure-app"
+    }
+   
+  }
+
+  spec {
+    replicas = 2
+
+    selector {
+      match_labels = {
+        test = "medicure-app"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          test = "medicure-app"
+        }
+      }
+
+      spec {
+        container {
+          image = "staragiledevops/medicure:1.0"
+          name  = "medicure"
+          
+      port {
+        container_port = 8083
+      }
+
+}
+}
+}
+}
+}
