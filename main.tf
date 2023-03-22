3aaedc3698ae9e6edc729bf3eaf6554235611618
StaragileDevops <116961807+StaragileDevops@users.noreply.github.com>
2023-03-22 21:46:08 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ terraform {
 }
 
 provider "kubernetes" {
-  config_path    = "/.kube/config"
+  config_path    = "~/.kube/config"
   config_context = "minikube"
 }
 resource "kubernetes_deployment" "medicure" {
